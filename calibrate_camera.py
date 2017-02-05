3cbb12b0fee6d503caeca40c5a432fb8f27140a7
hristo-vrigazov <hvrigazov@gmail.com>
2017-02-05 15:40:29 +0200

    Added tests for calibrated camera

--- calibrate_camera.py
+++ calibrate_camera.py
@@ -1,42 +1,67 @@
 import numpy as np
 import cv2
 import glob
+import pickle
+import os
 import matplotlib.pyplot as plt
 
 
-def calibrate_camera(nx, ny, calibration_images_pattern):
-    print('Calibrate camera')
-    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-    objp = np.zeros((nx * ny, 3), np.float32)
-    objp[:,:2] = np.mgrid[0:nx, 0:ny].T.reshape(-1,2)
+class CalibratedCamera:
 
-    # Arrays to store object points and image points from all the images.
-    objpoints = [] # 3d points in real world space
-    imgpoints = [] # 2d points in image plane.
+    def __init__(self, camera_calibration_pickle="camera_calibration_pickle.p", 
+        calibration_images_pattern="camera_cal/calibration*.jpg"):
+        if os.path.exists(camera_calibration_pickle):
+            self._calibrate_camera_by_pickle(camera_calibration_pickle)
+        else:
+            self._calibrate_camera_by_chessboard_images(9, 6, 
+                camera_calibration_pickle, calibration_images_pattern)
 
-    # Make a list of calibration images
-    images = glob.glob(calibration_images_pattern)
+    def _calibrate_camera_by_pickle(self, camera_calibration_pickle):
+        print('Reading calibrated camera parameters from pickle file')
+        with open(camera_calibration_pickle,"rb") as pickle_in:
+            dist_pickle = pickle.load(pickle_in)
 
-    # Step through the list and search for chessboard corners
-    for fname in images:
-        print(fname)
-        img = cv2.imread(fname)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        self.mtx = dist_pickle["mtx"]
+        self.dist = dist_pickle["dist"]
 
-        # Find the chessboard corners
-        ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
 
-        # If found, add object points, image points
-        if ret == True:
-            objpoints.append(objp)
-            imgpoints.append(corners)
+    def _calibrate_camera_by_chessboard_images(self, nx, ny, 
+            camera_calibration_pickle, calibration_images_pattern):
+        # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+        print('Calibrating camera')
+        objp = np.zeros((nx * ny, 3), np.float32)
+        objp[:,:2] = np.mgrid[0:nx, 0:ny].T.reshape(-1,2)
 
-            # Draw and display the corners
-            img = cv2.drawChessboardCorners(img, (nx, ny), corners, ret)
-            cv2.imshow('img',img)
-            cv2.waitKey(0)
+        # Arrays to store object points and image points from all the images.
+        objpoints = [] # 3d points in real world space
+        imgpoints = [] # 2d points in image plane.
 
-    cv2.destroyAllWindows()
+        # Make a list of calibration images
+        images = glob.glob(calibration_images_pattern)
 
-if __name__ == "__main__":
-    calibrate_camera(9, 6, 'camera_cal/calibration*.jpg')
+        # Step through the list and search for chessboard corners
+        for fname in images:
+            img = cv2.imread(fname)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            # Find the chessboard corners
+            ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
+
+            # If found, add object points, image points
+            if ret == True:
+                objpoints.append(objp)
+                imgpoints.append(corners)
+
+        img = cv2.imread("camera_cal/calibration1.jpg")
+        img_size = (img.shape[1], img.shape[0])
+
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size,None,None)
+        dist_pickle = {}
+        dist_pickle["mtx"] = mtx
+        dist_pickle["dist"] = dist
+
+        with open(camera_calibration_pickle, "wb") as pickle_file: 
+            pickle.dump(dist_pickle, pickle_file)
+
+        self.mtx = mtx
+        self.dist = dist
---
+++ test/test_calibrate_camera.py
@@ -0,0 +1,31 @@
+import unittest
+import os
+
+from calibrate_camera import CalibratedCamera
+
+class Test(unittest.TestCase):
+
+	def test_constuctor_sets_mtx_dist_if_pickle_is_not_available(self):
+		pickle_file_name = "something.p"
+		images_pattern = "camera_cal/calibration*.jpg"
+
+		os.remove(pickle_file_name)
+		calibrated_camera = CalibratedCamera(pickle_file_name, images_pattern)
+
+		self.assertIsNotNone(calibrated_camera.mtx)
+		self.assertIsNotNone(calibrated_camera.dist)
+
+
+	def test_constuctor_set_mtx_dist_from_pickle_if_available(self):
+		pickle_file_name = "something.p"
+		images_pattern = "camera_cal/calibration*.jpg"
+
+		os.remove(pickle_file_name)
+
+		# create the pickle
+		CalibratedCamera(pickle_file_name, images_pattern)
+
+		# now calibrated camera sets those from pickle
+		calibrated_camera = CalibratedCamera(pickle_file_name, images_pattern)
+		self.assertIsNotNone(calibrated_camera.mtx)
+		self.assertIsNotNone(calibrated_camera.dist)
