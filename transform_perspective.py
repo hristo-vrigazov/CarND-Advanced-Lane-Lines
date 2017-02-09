63436d8cd97ed9a43bdc4bc715c35dc9a3666eb3
hristo-vrigazov <hvrigazov@gmail.com>
2017-02-09 20:45:07 +0200

    Implemented perspective transformer

---
+++ transform_perspective.py
@@ -0,0 +1,15 @@
+import cv2
+import pickle
+
+class PerspectiveTransormer:
+	def __init__(self, src, dst):
+		self.M = cv2.getPerspectiveTransform(src, dst)
+		self.Minv = cv2.getPerspectiveTransform(dst, src)
+
+
+    def transform(self, img):
+        return cv2.warpPerspective(img, self.M, (img.shape[1], img.shape[0]), flags=cv2.INTER_LINEAR)
+
+
+    def inverse_transform(self, img):
+        return cv2.warpPerspective(img, self.M_inv, (img.shape[1], img.shape[0]), flags=cv2.INTER_LINEAR)
